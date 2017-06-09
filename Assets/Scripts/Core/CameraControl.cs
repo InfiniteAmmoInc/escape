7ce0138c9e14b2818abe4c1f4b90c4606d9a3c97
Alec Holowka <zaphire@gmail.com>
2017-06-08 21:40:09 -0500

    title screen

--- Assets/Scripts/Core/CameraControl.cs
+++ Assets/Scripts/Core/CameraControl.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public new Camera camera;
-    public float followSpeed = 1f;
+    public float smoothTime = 0.3f;
     public float lookAhead = 0f;
     public float playerVelocitySpeed = 1f;
     Player player;
     Vector3 playerVelocity;
+    Vector3 cameraVelocity;
 
     void Start()
     {
@@ -20,10 +21,14 @@ public class CameraControl : MonoBehaviour
     {
         if (player != null)
         {
-            playerVelocity = Vector3.Lerp(playerVelocity, player.GetComponent<Mover>().velocity, Time.deltaTime * playerVelocitySpeed);
+            //playerVelocity = Vector3.Lerp(playerVelocity, player.GetComponent<Mover>().velocity, Time.deltaTime * playerVelocitySpeed);
             var targetPosition = player.transform.position + playerVelocity * lookAhead;
+
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref cameraVelocity, smoothTime);
+            /*
             float distance = (targetPosition - transform.position).magnitude;
             transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * followSpeed * distance);
+            */
         }
     }
 }
---
+++ Assets/Scripts/Title/Title.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title : MonoBehaviour
+{
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Application.LoadLevel("Game");
+		}
+	}
+}
